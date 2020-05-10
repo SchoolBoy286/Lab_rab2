bf00bdd1a9203608a24bce1b80a637a746a748d2
SchoolBoy286 <dmitriywhite286@gmail.com>
2020-05-10 17:39:02 +0300

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Second commit");
-        System.out.println("Third commit");
+        System.out.println("Third commit1");
     }
 
 }
